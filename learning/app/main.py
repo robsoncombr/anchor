456ff1744bf9a614e6ca30b91a7d17aa24195ee0
Robson Martins <robson@robson.com.br>
2022-06-16 12:52:19 -0300

    learning: variable rules

--- learning/app/main.py
+++ learning/app/main.py
@@ -1,13 +1,21 @@
-from flask import Flask
+# https://flask.palletsprojects.com/en/1.1.x/quickstart/
+
+from flask import Flask, url_for
 from markupsafe import escape
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return 'Index'
+    return 'index'
 
-@app.route('/<parm>')
+@app.route('/parm/<parm>')
 def withParm(parm):
   #python 2,7 still does not support f strings
-  return 'parm: %s' % escape(parm)
+  #return 'parm: %s' % escape(parm)
+  #with format
+  return 'parm: {}'.format(escape(parm))
+
+@app.route('/int/<int:parm>') # if not int return 404
+def withParmType(parm):
+  return 'int: %s' % parm
